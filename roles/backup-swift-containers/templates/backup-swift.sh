9c9c42b4c4437cca81015d179b238ff222d8c5d0
Jacek Bzdak <jacek@opencraft.com>
2016-04-16 15:53:35 +0200

    Renaames

--- roles/backup-swift-containers/templates/backup-swift.sh
+++ roles/backup-swift-containers/templates/backup-swift.sh
@@ -6,7 +6,7 @@ set -e
 timestamp=$(date +%Y%m%dT%H:%M:%S)
 
 {{ LOCAL_SWIFT_SHELL_COMMAND }} download -D "{{ BACKUP_SWIFT_LOCAL_DIR }}"  "{{BACKUP_SWIFT_CONTAINER}}"
-/usr/local/bin/tarsnap -c --keyfile "{{ tarsnap_key }}" --cachedir "{{ tarsnap_cache }}" -f "{{ BACKUP_SWIFT_BACKUP_NAME }}-${timestamp}" "{{ BACKUP_SWIFT_LOCAL_DIR }}"
+/usr/local/bin/tarsnap -c --keyfile "{{ tarsnap_keyfile }}" --cachedir "{{ tarsnap_cache }}" -f "{{ BACKUP_SWIFT_BACKUP_NAME }}-${timestamp}" "{{ BACKUP_SWIFT_LOCAL_DIR }}"
 {% if BACKUP_SWIFT_SNITCH %}
 curl "{{ BACKUP_SWIFT_SNITCH }}"
 {% endif %}
--- roles/mysql/README.md
+++ roles/mysql/README.md
@@ -1,8 +1,7 @@
 Mysql
 =====
 
-A role that attaches ``/var/lib/mysql`` to a OpenStack volume, it also installs MYSQL from mysql apt repositories. 
-
+A role that attaches ``/var/lib/mysql`` to a OpenStack volume.
 Role Variables
 --------------
 
